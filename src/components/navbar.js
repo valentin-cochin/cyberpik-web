456d98e830070772059f4e209412ffad1ad28cb8
valentin-cochin <cochin.valentin@hotmail.fr>
2021-04-16 15:57:38 +0200

    Remove blog elements and update Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -31,17 +31,6 @@ class Navbar extends React.Component {
 							<li className="nav-item">
 								<a className="nav-link" href="#team">team</a>
 							</li>
-							<li className="nav-item dropdown">
-								<a className="nav-link dropdown-toggle" href="#blog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">blog</a>
-								<ul className="dropdown-menu">
-									<li className="nav-item"><a className="nav-link" href={`${process.env.PUBLIC_URL}/blog-list`} target="_blank">blog list</a></li>
-									<li className="nav-item"><a className="nav-link" href={`${process.env.PUBLIC_URL}/blog-details`} target="_blank">blog details</a></li>
-									<li className="nav-item"><a className="nav-link" href={`${process.env.PUBLIC_URL}/blog-leftside`} target="_blank">leftsidebar</a></li>
-									<li className="nav-item"><a className="nav-link" href={`${process.env.PUBLIC_URL}/blog-rightside`} target="_blank">rightsidebar</a></li>
-									<li className="nav-item"><a className="nav-link" href={`${process.env.PUBLIC_URL}/blog-left-sidebar`} target="_blank">details leftsidebar</a></li>
-									<li className="nav-item"><a className="nav-link" href={`${process.env.PUBLIC_URL}/blog-right-sidebar`} target="_blank">details rightsidebar</a></li>
-								</ul>
-							</li>
 							<li className="nav-item">
 								<a className="nav-link" href="#price" data-menuanchor="price">price</a>
 							</li>
