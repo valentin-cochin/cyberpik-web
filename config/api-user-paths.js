0ad2c733c93de9253c33b4622d70de50af1f654f
Antoine FRANCOIS <francois.antoine3@gmail.com>
2021-04-19 15:45:56 +0200

    Set the constant URL to access to the API User and the other ones

---
+++ config/api-user-paths.js
@@ -0,0 +1,11 @@
+// URL to access to the API User
+export const BACKEND_URL = 'https://localhost:8080/cyberpik'
+
+// URL to acces to the differents API User controller
+export const USER_ACCOUNT_URL = '/user-accounts'
+
+// API for countries, states & cities requests URL
+export const CITY_COUNTRY_URL = 'https://www.universal-tutorial.com/api'
+export const COUNTRY_URL = '/countries/'
+export const STATE_URL = '/states/'
+export const CITY_URL = '/cities/'
