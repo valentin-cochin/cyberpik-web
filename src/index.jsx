69c9d2d99b207b079301408e5017d95ccb016b81
valentin-cochin <cochin.valentin@hotmail.fr>
2021-04-20 10:38:58 +0200

    Add project presentation

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.scss';
 import { default as NoMatch } from './pages/404';
-import Faq from './pages/faq';
 import ForgetPassword from './pages/forget-pwd';
 import Home from './pages/home';
-import Review from './pages/review';
+import ProjectPresentation from './pages/project-presentation';
 import SignIn from './pages/sign-in';
 import SignUp from './pages/sign-up';
-import ThankYou from './pages/thank-you';
 import registerServiceWorker from './registerServiceWorker';
 
 class Root extends React.Component {
@@ -21,9 +19,7 @@ class Root extends React.Component {
 			  <Route path={`${process.env.PUBLIC_URL}/sign-in`} component={SignIn}/>
 			  <Route path={`${process.env.PUBLIC_URL}/sign-up`} component={SignUp}/>
 			  <Route path={`${process.env.PUBLIC_URL}/forget-password`} component={ForgetPassword}/>
-			  <Route path={`${process.env.PUBLIC_URL}/thank-you`} component={ThankYou}/>
-			  <Route path={`${process.env.PUBLIC_URL}/review`} component={Review}/>
-			  <Route path={`${process.env.PUBLIC_URL}/faq`} component={Faq}/>
+			  <Route path={`${process.env.PUBLIC_URL}/project-presentation`} component={ProjectPresentation}/>
 			  <Route component={NoMatch} />
 			</Switch>
 		</BrowserRouter>
---
+++ src/pages/project-presentation.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Footer from '../components/footer';
+import Navbar from '../components/navbar';
+import Team from '../components/team';
+
+class ProjectPresentation extends React.Component {
+	componentDidMount() {
+		setTimeout(function () {
+			document.querySelector(".loader-wrapper").style = "display: none";
+		}, 2000);
+	}
+	render() {
+		return (
+			<div>
+				<Navbar />
+				<div className="page-margin">
+
+					<div className="breadcrumb-bg">
+						<div className="container">
+							<div className="row">
+								<div className="col-md-6 col-sm-6 col-text-center d-align-center">
+									<h2 className="title">Project<span> Presentation</span></h2>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<section>
+						<div className="container">
+							<div className="row">
+								<div className="col-sm-12">
+									<div className="blog-item">
+										<div className="blog-block">
+											<div className="blog-box">
+												<div className="overflow-hidden">
+													<a href={null}>
+														<img src="assets/images/project_presentation_header.png" alt="Project presentation" className="img-fluid" />
+													</a>
+												</div>
+											</div>
+										</div>
+										<div className="blog-text">
+                                            <h2 className="title"><br/>About <span>Cyberpik</span></h2>
+											<div className="blog-divider"></div>
+											<div className="blog-description">
+												<p>Cyperpik is for technophiles who like cyberpunk, vaporwave, synthwave, etc. CyberPik allows them to have fun customising their photos in these themes.</p>
+                                                <p>The idea was born in 2020 as the project to validate a developer training. This app serves also as a mean to demonstrate our technical skills and soft skills to future employers.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+				</div>
+                <Team/>
+                <Footer/>
+			</div>
+		);
+	}
+}
+
+export default ProjectPresentation;
