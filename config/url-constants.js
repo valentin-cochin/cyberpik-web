1641f0f5e7d950613d04edc88f0d8786e68a5949
Antoine FRANCOIS <francois.antoine3@gmail.com>
2021-04-20 13:41:46 +0200

    Create the url constants of the front to navigate in the website

---
+++ config/url-constants.js
@@ -0,0 +1,20 @@
+// URL to access to the API User
+export const BACK_END_URL = 'http://localhost:8080/cyberpik'
+
+// URL to access to the different API User controllers
+export const USER_ACCOUNT_BACK_URL = BACK_END_URL + '/user_accounts'
+
+// URL to navigate between the different front pages
+export const HOME_PAGE = '/'
+export const SIGN_IN = '/sign-in'
+export const SIGN_UP = '/sign-up'
+export const FORGET_PASSWORD = '/forget-password'
+export const PROJECT_PRESENTATION = '/project-presentation'
+export const THANK_YOU = '/thank-you'
+export const GENERAL_CONDITIONS = '/general-conditions'
+
+// API for countries, states & cities requests URL
+export const CITY_COUNTRY_URL = 'https://www.universal-tutorial.com/api'
+export const COUNTRY_URL = '/countries/'
+export const STATE_URL = '/states/'
+export const CITY_URL = '/cities/'
