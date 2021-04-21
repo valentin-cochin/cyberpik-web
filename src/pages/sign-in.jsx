82b55061e46854f16e4384244945a2ffcae99479
Antoine FRANCOIS <francois.antoine3@gmail.com>
2021-04-21 21:34:35 +0200

    Set the signin page

--- src/pages/sign-in.jsx
+++ src/pages/sign-in.jsx
@@ -1,58 +1,78 @@
 import React from 'react';
+import axios from 'axios';
+import { Link, useHistory } from 'react-router-dom';
+import { Form, Formik } from 'formik';
+import { BACK_END_SIGN_IN, FORGET_PASSWORD, HOME_PAGE, SIGN_UP } from '../../config/url-constants';
+import { Email } from '../components/user_accounts/email';
+import { Password } from '../components/user_accounts/password';
+import { signInInitialValues } from '../shared/initialValues';
+import { signInValidationSchema } from '../shared/validationSchemas';
 
 
-class SignIn extends React.Component {
+const SignIn = () => {
+   const history = useHistory()
 
-   componentDidMount() {
-      setTimeout(function () {
-         document.querySelector(".loader-wrapper").style = "display: none";
-      }, 2000);
+   setTimeout(function () {
+      document.querySelector(".loader-wrapper").style = "display: none";
+   }, 2000);
+
+   const submit = (values) => {
+      axios.post(BACK_END_SIGN_IN, {
+         email: values.email,
+         password: values.password
+      }).then(res => {
+         history.push(HOME_PAGE)
+      }).catch(err => {
+         document.getElementById("error_message_sign_in").innerHTML = err.response.data
+      })
+
    }
 
-   render() {
+   return (
+      <section className="authentication-form">
+         <div className="innerpage-decor">
+            <div className="innerpage-circle1"><img src="assets/images/Testimonial2.png" alt="" /></div>
+            <div className="innerpage-circle2"><img src="assets/images/Testimonial1.png" alt="" /></div>
+         </div>
+         <div>
+            <h2 className="title text-center"><span>Login</span></h2>
+            <p className="text-center">Welcome to <span>CYBERPIK</span>, Please Login with your personal account information.</p>
+            <div className="card">
 
-      return (
-         <section className="authentication-form">
-            <div className="innerpage-decor">
-               <div className="innerpage-circle1"><img src="assets/images/Testimonial2.png" alt="" /></div>
-               <div className="innerpage-circle2"><img src="assets/images/Testimonial1.png" alt="" /></div>
-            </div>
-            <div>
-               <h2 className="title text-center"><span> Login</span></h2>
-               <p className="text-center">Welcome to tovo, Please Login with your personal account information.</p>
-               <div className="card">
-                  <form className="theme-form">
-                     <div className="form-group">
-                        <input type="email" className="form-control" placeholder="email address" required="required" />
-                     </div>
-                     <div className="form-group">
-                        <input required="" name="login[password]" type="password" className="form-control" placeholder="Password" />
-                        <div className="show-hide">
-                           <span className="show"></span>
-                        </div>
-                     </div>
+               <Formik initialValues={signInInitialValues}
+                  onSubmit={submit}
+                  validationSchema={signInValidationSchema}>
+                  <Form className="theme-form">
+                     <Email />
+                     <Password />
                      <div className="form-group row">
                         <div className="custom-control custom-checkbox col-6">
                            <input type="checkbox" className="custom-control-input" id="customControlAutosizing" />
                            <label className="custom-control-label" htmlFor="customControlAutosizing">Remember me</label>
                         </div>
-                        <a href={`${process.env.PUBLIC_URL}/forget-password`} className="text-right col-6 theme-link">lost your password</a>
+                        <a href={FORGET_PASSWORD} className="text-right col-6 theme-link">lost your password</a>
                      </div>
+                     <div id="error_message_sign_in" className="text-center mt-0 mb-3"></div>
                      <div className="form-button text-center">
                         <button type="submit" className="btn btn-custom btn-lg theme-color">Login</button>
                      </div>
-                     <div className="or-saparator"><span>or</span></div>
-                     <h6 className="text-center mt-0 mb-3">Sign in with:</h6>
-                     <div className="form-button text-center social-btns">
-                        <button type="submit" className="btn btn-custom fb">Facebook</button>
-                        <button type="submit" className="btn btn-custom ggl">Google</button>
+                     <br />
+                     <div className="form-button text-center">
+                        <button className="btn btn-custom theme-color"><Link className="text-white" to={SIGN_UP}>Doesn't have an account yet?</Link></button>
                      </div>
-                  </form>
+                  </Form>
+               </Formik>
+
+               <div className="or-saparator"><span>or</span></div>
+               <h6 className="text-center mt-0 mb-3">Sign in with:</h6>
+               <div className="form-button text-center social-btns">
+                  <button type="submit" className="btn btn-custom fb">Facebook</button>
+                  <button type="submit" className="btn btn-custom ggl">Google</button>
                </div>
             </div>
-         </section>
-      );
-   }
+         </div>
+      </section>
+   );
 }
 
 
