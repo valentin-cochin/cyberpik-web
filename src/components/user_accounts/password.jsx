564ee57c0f79a1b4074a64e5cb92c32f0b6ab258
Antoine FRANCOIS <francois.antoine3@gmail.com>
2021-04-22 10:59:43 +0200

    Delete the possibility to see the password

--- src/components/user_accounts/password.jsx
+++ src/components/user_accounts/password.jsx
@@ -6,9 +6,6 @@ export const Password = () => {
     return (
         <div className="form-group">
             <Field type="password" name="password" className="form-control" placeholder="Password" required="required" />
-            <div className="show-hide">
-                <span className="show"></span>
-            </div>
             <ErrorMessage componant="small" name="password" className="ml-5 mt-0 mb-3" />
         </div>
     )
