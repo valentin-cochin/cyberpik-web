cad72ca782256f09043a4a9e26c22d8c4a8089a0
valentin-cochin <cochin.valentin@hotmail.fr>
2021-04-26 10:23:09 +0200

    Order urls in url-constants

--- src/pages/filter-choice.jsx
+++ src/pages/filter-choice.jsx
@@ -1,6 +1,4 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import Dropzone from "react-dropzone"
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { HOME_PAGE } from '../../config/url-constants'
 
@@ -20,33 +18,7 @@ const Import = () => {
         backgroundColor: '#eeeeee',
     };
 
-    useEffect(() => {
-        setTimeout(function () {
-            document.querySelector(".loader-wrapper").style = "display: none"
-        }, 1200);
-    })
 
-    const onDrop = (acceptedFiles) => {
-        acceptedFiles.forEach((file) => {
-
-            let formData = new FormData()
-            formData.append("file", file)
-
-            axios.post("http://localhost:8080/images/", formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            })
-                .then(response => {
-                    console.log(response.data);
-                    // TODO: Add preview code here
-                })
-                .catch((error) => {
-                    console.log(error)
-                })
-        })
-
-    }
 
     return (
         <section className="authentication-form download">
@@ -60,23 +32,8 @@ const Import = () => {
                         <div className="text-center">
                             <i className="fa fa-cloud-upload" aria-hidden="true" style={{ fontSize: "10rem", color: "#777777" }}></i>
                             <div className="col-lg-8 offset-lg-2">
-                                <h2>Import your image</h2>
+                                <h2>Choose the effect</h2>
                             </div>
-                            <Dropzone
-                                onDrop={onDrop}
-                                accept=".jpeg,.jpg"
-                                maxFiles={1}
-                                minSize={1024}
-                                maxSize={3072000}
-                            >
-                                {({ getRootProps, getInputProps }) => (
-                                    <div {...getRootProps({ style })}>
-                                        <input {...getInputProps()} />
-                                        <h3>Drag 'n' drop your image here, or click to select it</h3>
-                                        <em>(Only *.jpeg and *.jpg images will be accepted)</em>
-                                    </div>
-                                )}
-                            </Dropzone>
                         </div>
                     </div>
                 </div>
