004b1f236957988dab4796236b255f8324f25863
Antoine FRANCOIS <francois.antoine3@gmail.com>
2021-04-26 09:23:08 +0200

    Set the routes for the profile & the profile manager pages

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,9 @@ import SignUp from './pages/sign-up';
 import Import from './pages/import';
 import GeneralConditions from './pages/general-conditions';
 import registerServiceWorker from './registerServiceWorker';
-import { FORGET_PASSWORD, GENERAL_CONDITIONS, HOME_PAGE, PROJECT_PRESENTATION, SIGN_IN, SIGN_UP, IMPORT } from '../config/url-constants';
+import { FORGET_PASSWORD, GENERAL_CONDITIONS, HOME_PAGE, PROJECT_PRESENTATION, SIGN_IN, SIGN_UP, IMPORT, THANK_YOU, PROFILE, PROFILE_MANAGER } from '../config/url-constants';
+import UserAccountView from './pages/user-account-view';
+import UserAccountModify from './pages/user-account-modify';
 
 class Root extends React.Component {
   render() {
@@ -21,6 +23,8 @@ class Root extends React.Component {
 			  <Route exact path={`${process.env.PUBLIC_URL}${HOME_PAGE}`} component={Home}/>
 			  <Route path={`${process.env.PUBLIC_URL}${SIGN_IN}`} component={SignIn}/>
 			  <Route path={`${process.env.PUBLIC_URL}${SIGN_UP}`} component={SignUp}/>
+			  <Route path={`${process.env.PUBLIC_URL}${PROFILE}`} component={UserAccountView}/>
+			  <Route path={`${process.env.PUBLIC_URL}${PROFILE_MANAGER}`} component={UserAccountModify}/>
 			  <Route path={`${process.env.PUBLIC_URL}${FORGET_PASSWORD}`} component={ForgetPassword}/>
 			  <Route path={`${process.env.PUBLIC_URL}${PROJECT_PRESENTATION}`} component={ProjectPresentation}/>
 			  <Route path={`${process.env.PUBLIC_URL}${IMPORT}`} component={Import}/>
