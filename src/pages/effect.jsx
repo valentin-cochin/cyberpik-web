549296444b1a33fb2cdab1172a3aa09f6639e9bd
valentin-cochin <cochin.valentin@hotmail.fr>
2021-04-28 10:18:13 +0200

    Improvment of buttons in Effect.

--- src/pages/effect.jsx
+++ src/pages/effect.jsx
@@ -25,7 +25,7 @@ const Effect = () => {
                             <div className="col-lg-8 offset-lg-2">
                                 <h2>Choose the effect</h2>
                             </div>
-                            <EffectCarousel/>
+                            <EffectCarousel />
                         </div>
                     </div>
                 </div>
@@ -36,14 +36,14 @@ const Effect = () => {
                                 <div className="copyright-section">
                                     <p>This app was made with <span role="img" aria-label="heart">❤️</span> by <a href={GITHUB_VALENTIN} target="_blank" rel="noopener noreferrer">Valentin Cochin</a> and <a href={GITHUB_ANTOINE} target="_blank" rel="noopener noreferrer">Antoine François</a></p>
                                 </div>
-                                <div className="row d-flex justify-content-center align-items">
-                                    <div className="col-3">
-                                        <Link to={IMPORT} className="btn btn-custom btn-lg theme-color btn-back m-2"><i className="fa fa-angle-double-left mr-2"></i>Import</Link>
-                                    </div>
-                                    <div className="col-3">
-                                        <Link to={DOWNLOAD} className="btn btn-custom btn-lg theme-color btn-back m-2"><i className="fa fa-angle-double-right mr-2"></i>Transform</Link>
-                                    </div>
-                                </div>
+                            </div>
+                        </div>
+                        <div className="row d-flex justify-content-around">
+                            <div className="col-3">
+                                <Link to={IMPORT} className="btn btn-custom btn-lg theme-color btn-back m-2"><i className="fa fa-angle-double-left mr-2"></i>Import</Link>
+                            </div>
+                            <div className="col-3">
+                                <Link to={DOWNLOAD} className="btn btn-custom btn-lg theme-color btn-back m-2"><i className="fa fa-angle-double-right mr-2"></i>Transform</Link>
                             </div>
                         </div>
                     </div>
