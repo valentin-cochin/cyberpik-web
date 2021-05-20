a4170d2e1696d2234be334f04008f725286d1970
Valentin <cochin.valentin@hotmail.fr>
2021-05-20 13:30:32 +0200

    Add download pop-up to download.jsx

--- src/pages/download.jsx
+++ src/pages/download.jsx
@@ -1,14 +1,29 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { GITHUB_ANTOINE, GITHUB_VALENTIN, HOME_PAGE } from '../shared/url-constants';
+import FileSaver from 'file-saver';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { GITHUB_ANTOINE, GITHUB_VALENTIN, HOME_PAGE, IMPORT } from '../shared/url-constants';
 
 const Download = () => {
+    const [transformedImage, setTransformedImage] = useState(0)
+    const location = useLocation()
+    const history = useHistory()
 
     useEffect(() => {
         setTimeout(function () {
             document.querySelector(".loader-wrapper").style = "display: none"
         }, 1200);
     })
+
+    useEffect(() => {
+        if (!location.state) {
+            history.push(IMPORT)
+        } else {
+            setTransformedImage(location.state.transformedImage)
+            if (transformedImage !== 0) {
+                FileSaver.saveAs(new Blob([transformedImage.blob]), "tranformed_image.jpg")
+            }
+        }
+    }, [location.state, history, transformedImage])
 
     return (
         <section className="authentication-form download">
@@ -24,7 +39,6 @@ const Download = () => {
                             <div className="col-lg-8 offset-lg-2">
                                 <h2>Your download should begin automatically</h2>
                             </div>
-                            <h3>If it doesn’t start automatically, <a href="" className="manual-down">please click here to downlaod manually.</a></h3>
                         </div>
                     </div>
                 </div>
@@ -33,7 +47,7 @@ const Download = () => {
                         <div className="row">
                             <div className="col-md-12">
                                 <div className="copyright-section">
-                                <p>This app was made with <span role="img" aria-label="heart">❤️</span> by <a href={GITHUB_VALENTIN} target="_blank" rel="noopener noreferrer">Valentin Cochin</a> and <a href={GITHUB_ANTOINE} target="_blank" rel="noopener noreferrer">Antoine François</a></p>
+                                    <p>This app was made with <span role="img" aria-label="heart">❤️</span> by <a href={GITHUB_VALENTIN} target="_blank" rel="noopener noreferrer">Valentin Cochin</a> and <a href={GITHUB_ANTOINE} target="_blank" rel="noopener noreferrer">Antoine François</a></p>
                                 </div>
                                 <div className="form-button text-center">
                                     <Link to={HOME_PAGE} className="btn btn-custom btn-lg theme-color btn-back"><i className="fa fa-angle-double-left mr-2"></i>Back to home</Link>
